cafbb59f73133a1211d7bf23375f60d554f33c3e
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2016-11-16 14:36:13 +0530

    Refactor SnippetList view using ListCreateAPIView

--- snippets/views.py
+++ snippets/views.py
@@ -1,19 +1,7 @@
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import Http404
+from rest_framework import generics
 
-class SnippetList(APIView):
-    def get(self, request):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data, status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_201_CREATED)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
